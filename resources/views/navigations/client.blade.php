bbaf54442b37f698b980019691f76d0233fbc6ec
Thanyani <khalishisimon@gmail.com>
2026-03-21 14:23:24 +0200

    add navigation items

--- resources/views/navigations/client.blade.php
+++ resources/views/navigations/client.blade.php
@@ -32,7 +32,7 @@
           <div
             class="px-3 pt-5 pb-2 text-xs font-semibold tracking-wider text-gray-500 uppercase"
           >
-            Projects
+            Navigation
           </div>
           <a
             href="javascript:void(0)"
@@ -57,11 +57,7 @@
                 />
               </svg>
             </span>
-            <span class="grow py-2">Manage</span>
-            <span
-              class="inline-flex rounded-full border border-gray-700 bg-gray-700 px-1.5 py-0.5 text-xs leading-4 font-semibold text-gray-50"
-              >99+</span
-            >
+            <span class="grow py-2">Agents</span>
           </a>
           <a
             href="javascript:void(0)"
@@ -86,11 +82,7 @@
                 />
               </svg>
             </span>
-            <span class="grow py-2">Tasks</span>
-            <span
-              class="inline-flex rounded-full border border-gray-700 bg-gray-700 px-1.5 py-0.5 text-xs leading-4 font-semibold text-gray-50"
-              >9</span
-            >
+            <span class="grow py-2">Script Editor</span>
           </a>
           <a
             href="javascript:void(0)"
@@ -115,11 +107,7 @@
                 />
               </svg>
             </span>
-            <span class="grow py-2">Clients</span>
-            <span
-              class="inline-flex rounded-full border border-gray-700 bg-gray-700 px-1.5 py-0.5 text-xs leading-4 font-semibold text-gray-50"
-              >26</span
-            >
+            <span class="grow py-2">Knowledge Base</span>
           </a>
           <a
             href="javascript:void(0)"
@@ -144,13 +132,9 @@
                 />
               </svg>
             </span>
-            <span class="grow py-2">Add New</span>
+            <span class="grow py-2">Integrations</span>
           </a>
-          <div
-            class="px-3 pt-5 pb-2 text-xs font-semibold tracking-wider text-gray-500 uppercase"
-          >
-            Account
-          </div>
+        
           <a
             href="javascript:void(0)"
             class="group flex items-center gap-2 rounded-lg border border-transparent px-2.5 text-sm font-medium text-gray-200 hover:bg-gray-700/75 hover:text-white active:border-gray-600"
@@ -174,62 +158,7 @@
                 />
               </svg>
             </span>
-            <span class="grow py-2">Profile</span>
-          </a>
-          <a
-            href="javascript:void(0)"
-            class="group flex items-center gap-2 rounded-lg border border-transparent px-2.5 text-sm font-medium text-gray-200 hover:bg-gray-700/75 hover:text-white active:border-gray-600"
-          >
-            <span
-              class="flex flex-none items-center text-gray-500 group-hover:text-gray-300"
-            >
-              <svg
-                class="hi-outline hi-cog-8-tooth inline-block size-5"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-            </span>
-            <span class="grow py-2">Settings</span>
-          </a>
-          <a
-            href="javascript:void(0)"
-            class="group flex items-center gap-2 rounded-lg border border-transparent px-2.5 text-sm font-medium text-gray-200 hover:bg-gray-700/75 hover:text-white active:border-gray-600"
-          >
-            <span
-              class="flex flex-none items-center text-gray-500 group-hover:text-gray-300"
-            >
-              <svg
-                class="hi-outline hi-lock-closed inline-block size-5"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-                />
-              </svg>
-            </span>
-            <span class="grow py-2">Log out</span>
+            <span class="grow py-2">Call Logs & Analytics</span>
           </a>
         </nav>
       </div>
