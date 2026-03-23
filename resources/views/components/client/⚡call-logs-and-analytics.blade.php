0d6a9d9faa8a503d11db5e0ab1c925157cd8cc6a
Thanyani <khalishisimon@gmail.com>
2026-03-23 19:26:16 +0200

    add phone icon

--- resources/views/components/client/⚡call-logs-and-analytics.blade.php
+++ resources/views/components/client/⚡call-logs-and-analytics.blade.php
@@ -123,8 +123,22 @@ new class extends Component
                 class="border-b border-gray-100 dark:border-gray-700/50"
             >
                 <td class="py-3 pr-3 font-medium">2026-03-23</td>
-                <td class="p-3 text-gray-500 dark:text-gray-400">
-                +1234567890
+                <td class="flex items-center gap-2 p-3 text-gray-800 dark:text-gray-200 text-xl">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" viewBox="0 0 24 24" 
+                    stroke-width="1.5" 
+                    stroke="currentColor" 
+                    class="hi-outline hi-phone-arrow-down-left inline-block size-5 text-green-500"
+                    >
+                 <path 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round" 
+                    d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
+                    />
+                    </svg>
+                    +1234567890
+                    </div>
                 </td>
                 <td class="p-3">John Doe</td>
                 <td class="p-3">
